6cd12340cbad3ad3b4d6e579ca3e5c01e81d574f
Hyun Juwon <e215760@ie.u-ryukyu.ac.jp>
2023-04-27 16:00:58 +0900

    1_1

--- test_dataset1.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from dataset1 import true_function
-
-@pytest.mark.parametrize(('number','expected'),[
-    (0,123123),
-])
-def test_true_function(number,expected):
-    assert true_function(number) == expected
